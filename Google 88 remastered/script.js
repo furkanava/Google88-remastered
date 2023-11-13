b20008003bb2ed4c26135b1790222505b5f4c718
Ömer Avcı <111347166+oomeravcii@users.noreply.github.com>
2023-11-13 18:35:20 +0300

    Update script.js

--- Google 88 remastered/script.js
+++ Google 88 remastered/script.js
@@ -1,11 +1,12 @@
-// Written by oomeravcii on GitHub!
-
 let search_input;
 
 document.getElementById("searchButton").onclick = function(){
 
-    search_input = document.getElementById("searchInput").value
-
-    window.location.replace("https://www.google.com/search?q=" + search_input);
-
+    search_input = document.getElementById("searchInput").value;
+    if (search_input == ""){
+        window.alert("Input field can't be empty.");
+    }
+    else{
+        window.location.replace("https://www.google.com/search?q=" + search_input);
+    }
 }
